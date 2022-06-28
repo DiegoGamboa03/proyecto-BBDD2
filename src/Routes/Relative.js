33a41115afc5be5274ad99061b2c5c97afa5b2d6
Diego <diego.gamboaa03@gmail.com>
2022-06-28 13:08:11 -0400

    Añade y Elimina familiar

--- src/Routes/Relative.js
+++ src/Routes/Relative.js
@@ -30,4 +30,52 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    const sql = `SELECT * FROM Permisos WHERE Cedula = ${id}`;
+    conn.query(sql, (error, result) => {
+      if (error) throw error;
+  
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.send('Not result');
+      }
+    });
+  });
+  
+  router.post('/add', (req, res) => {
+    const sql = 'INSERT INTO Familiares SET ?';
+  
+    const workRestLeaveObj = {
+      CedulaTrabajador: req.body.CedulaTrabajador,
+      Cedula: req.body.Cedula,
+      PrimerNombre: req.body.PrimerNombre,
+      SegundoNombre: req.body.SegundoNombre,
+      PrimerApellido: req.body.PrimerApellido,
+      SegundoApellido: req.body.SegundoApellido,
+      FechaNacimiento: req.body.FechaNacimiento,
+      Genero: req.body.Genero,
+      Parentesco: req.body.Parentesco,
+    };
+    
+    // Aqui poner las verificaciones
+    
+    conn.query(sql, workRestLeaveObj, error => {
+      if (error) throw error;
+      res.send('Relative created!');
+    });
+  });
+
+  router.delete('/delete/:id', (req, res) => {
+    const { id, date } = req.params;
+    const sql = `DELETE FROM Familiares WHERE Cedula = '${id}'`;
+  
+    conn.query(sql, error => {
+      if (error) throw error;
+      res.send('Relative deleted');
+    });
+  });
+
 module.exports = router;
